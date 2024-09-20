1e7dd4188d6fe9559a599567a857f9f478fba273
toshiotm <toshi.otm@gmail.com>
2024-09-20 12:24:29 +0900

    first commit

---
+++ docker/db/init/init-user.sql
@@ -0,0 +1,3 @@
+create table user( id int, name varchar(32), email varchar(32) );
+insert user (id, name, email) values (1, 'test1', 'test1@example.com');
+insert user (id, name, email) values (2, 'test2', 'test2@example.com');
